7803af7c478a4a93d92a2d658b81b4f76c7a9915
kry shitenkova <shitenkovakris@mail.ru>
2023-01-10 19:36:58 +0300

    solve task16

---
+++ task16/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("IQ первого космонавта:")
+	var iq1 int
+	fmt.Scan(&iq1)
+
+	fmt.Println("iq второго космонавта:")
+	var iq2 int
+	fmt.Scan(&iq2)
+
+	fmt.Println("iq третьего космонавта:")
+	var iq3 int
+	fmt.Scan(&iq3)
+
+	max := iq1
+
+	if max < iq2 {
+		max = iq2
+	} else if max < iq3 {
+		max = iq3
+	}
+
+	fmt.Println(max)
+
+}
