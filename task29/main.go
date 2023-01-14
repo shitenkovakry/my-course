c9fde8a9cf1e7fc069780ad424c9f9c8c1bf9794
kry shitenkova <shitenkovakris@mail.ru>
2023-01-15 00:25:39 +0300

    solve task29

---
+++ task29/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := -1
+
+	for {
+		number++
+		if number > 10 {
+			break
+		}
+
+		if number%2 != 0 {
+			continue
+		}
+
+		fmt.Println(number, "кратен 2")
+	}
+}
+
+func main1() {
+	for number := 0; number <= 10; number++ {
+		if number%2 != 0 {
+			continue
+		}
+
+		fmt.Println(number, "кратен 2")
+	}
+}
