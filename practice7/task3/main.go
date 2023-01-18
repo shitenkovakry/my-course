2ed63b2dd5ad4c581a033f696f5a1734e592ad72
kry shitenkova <shitenkovakris@mail.ru>
2023-01-18 18:18:03 +0300

    solve task3 from ptactice7

---
+++ practice7/task3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Введите высоту елочки:")
+	var height int = 6
+	//fmt.Scan(&height)
+
+	for row := 0; row < height; row++ {
+		toPrintRow := ""
+		spaces := height - row - 1
+
+		totalSpaces := ""
+
+		for space := 0; space < spaces; space++ {
+			totalSpaces += "-"
+		}
+
+		totalStars := "*"
+		stars := row * 2
+
+		for star := 0; star < stars; star++ {
+			totalStars += "*"
+		}
+
+		toPrintRow = totalSpaces + totalStars + totalSpaces
+		fmt.Println(toPrintRow)
+	}
+}
