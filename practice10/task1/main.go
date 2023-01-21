4ed9c89627ec949aa11fc2a029c49c026966d97c
kry shitenkova <shitenkovakris@mail.ru>
2023-01-21 18:15:40 +0300

    solve task1 from practice10

---
+++ practice10/task1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("введите степень x:")
+	var x float64
+	fmt.Scan(&x)
+
+	fmt.Println("введите n:")
+	var n int
+	fmt.Scan(&n)
+
+	ex := 1.0
+	xi := 1.0
+
+	for i := 1; i < n; i++ {
+		xi *= x / float64(i)
+		ex = ex + xi
+	}
+
+	fmt.Println(ex)
+}
