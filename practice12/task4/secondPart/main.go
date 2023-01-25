df4d24d1f9ad4b2d8e8a4ffef865305f6990c724
kry shitenkova <shitenkovakris@mail.ru>
2023-01-25 16:17:27 +0300

    solve secondPart of task4 from practice12

--- practice12/task4/secondPart/main.go
+++ practice12/task4/secondPart/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+)
 
+func main() {
+	fileName := "practice12/task4/secondPart/fileTask4.txt"
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
 }
