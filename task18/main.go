8afa694a1764a9b6aebea4fdf42fd788190b0a21
kry shitenkova <shitenkovakris@mail.ru>
2023-01-26 12:59:18 +0300

    update task18

--- task18/main.go
+++ task18/main.go
@@ -2,60 +2,79 @@ package main
 
 import "fmt"
 
+func CalculateCash(rub100, rub50, rub20, rub10, rub5, rub2, rub1, coint50, coint20, coint10, coint5, coint2, coint1 uint) float32 {
+	totalCash := float32(0)
+	totalCash += float32(rub100) * 100.0
+	totalCash += float32(rub50) * 50.0
+	totalCash += float32(rub20) * 20.0
+	totalCash += float32(rub10) * 10.0
+	totalCash += float32(rub5) * 5.0
+	totalCash += float32(rub2) * 2.0
+	totalCash += float32(rub1) * 1.0
+	totalCash += float32(coint50) * 0.5
+	totalCash += float32(coint20) * 0.2
+	totalCash += float32(coint10) * 0.1
+	totalCash += float32(coint5) * 0.05
+	totalCash += float32(coint2) * 0.02
+	totalCash += float32(coint1) * 0.0
+
+	return totalCash
+}
+
 func main() {
 	fmt.Println("Введите сколько 100 рублей:")
-	var rub100 float32
+	var rub100 uint
 	fmt.Scan(&rub100)
 
 	fmt.Println("Введите сколько 50 рублей:")
-	var rub50 float32
+	var rub50 uint
 	fmt.Scan(&rub50)
 
 	fmt.Println("Введите сколько 20 рублей:")
-	var rub20 float32
+	var rub20 uint
 	fmt.Scan(&rub20)
 
 	fmt.Println("Введите сколько 10 рублей:")
-	var rub10 float32
+	var rub10 uint
 	fmt.Scan(&rub10)
 
 	fmt.Println("Введите сколько 5 рублей:")
-	var rub5 float32
+	var rub5 uint
 	fmt.Scan(&rub5)
 
 	fmt.Println("Введите сколько 2-х рублей:")
-	var rub2 float32
+	var rub2 uint
 	fmt.Scan(&rub2)
 
 	fmt.Println("Введите сколько 1-го рубля:")
-	var rub1 float32
+	var rub1 uint
 	fmt.Scan(&rub1)
 
 	fmt.Println("Введите сколько 50 копеек:")
-	var coint50 float32
+	var coint50 uint
 	fmt.Scan(&coint50)
 
 	fmt.Println("Введите сколько 20 копеек:")
-	var coint20 float32
+	var coint20 uint
 	fmt.Scan(&coint20)
 
 	fmt.Println("Введите сколько 10 копеек")
-	var coint10 float32
+	var coint10 uint
 	fmt.Scan(&coint10)
 
 	fmt.Println("Введите сколько 5 копеек:")
-	var coint5 float32
+	var coint5 uint
 	fmt.Scan(&coint5)
 
 	fmt.Println("Введите сколько 2 копеек:")
-	var coint2 float32
+	var coint2 uint
 	fmt.Scan(&coint2)
 
 	fmt.Println("Введите сколько 1 копейки")
-	var coint1 float32
+	var coint1 uint
 	fmt.Scan(&coint1)
 
-	totalRun := (rub100 * 100) + (rub50 * 50) + (rub20 * 20) + (rub10 * 10) + (rub5 * 5) + (rub2 * 2) + (rub1 * 1) + (coint50 * 0.5) + (coint20 * 0.2) + (coint10 * 0.1) + (coint5 * 0.05) + (coint2 * 0.02) + (coint1 * 0.01)
+	result := CalculateCash(rub100, rub50, rub20, rub10, rub5, rub2, rub1, coint50, coint20, coint10, coint5, coint2, coint1)
 
-	fmt.Println("Всего в кассе:", totalRun, "белорусских рублей")
+	fmt.Println("Всего в кассе:", result, "белорусских рублей")
 }
