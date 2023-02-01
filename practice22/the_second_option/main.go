0a3cfd37e00caf8dd574f566380898449d757d0a
kry shitenkova <shitenkovakris@mail.ru>
2023-02-01 18:34:55 +0300

    solve the_second_option of task1 from practice22

--- practice22/the_second_option/main.go
+++ practice22/the_second_option/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+var (
+	ErrNoResult = errors.New("нет числа в массиве")
+)
+
+func FindNumberFromArray(array []int, value int) (int, error) {
+	for i := 0; i < len(array); i++ {
+		valueFromArray := array[i]
+
+		if valueFromArray == value {
+			return valueFromArray, nil
+		}
+	}
+
+	return 0, ErrNoResult
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var array []int
+
+	for i := 0; i < 10; i++ {
+		array = append(array, rand.Intn(10))
+	}
+	fmt.Println(array)
+
+	var value int
+	fmt.Scan(&value)
+
+	result, err := FindNumberFromArray(array, value)
+	if err != nil {
+		fmt.Println("нет числа в массиве")
+
+		return
+	}
+
+	fmt.Println(result)
 }
