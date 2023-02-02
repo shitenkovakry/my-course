ace6e192ef9cd8e2c13b1bd57df59d8d7153e1b8
kry shitenkova <shitenkovakris@mail.ru>
2023-02-02 11:33:15 +0300

    solve task2 from practice20

--- practice20/task2/main.go
+++ practice20/task2/main.go
@@ -1,5 +1,80 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func VectorXVector(vector1, vector2 []int) int {
+	summaOfVectors := 0
+
+	for i := 0; i < len(vector1) || i < len(vector2); i++ {
+		summaOfVectors += (vector1[i] * vector2[i])
+	}
+
+	return summaOfVectors
+}
+
+func ObtainColumnFromMatrix(inputColumn int, inputMatrix [][]int) []int {
+	newArray := []int{}
+
+	for i := 0; i < len(inputMatrix); i++ {
+		row := inputMatrix[i]
+		value := row[inputColumn]
+		newArray = append(newArray, value)
+	}
+
+	return newArray
+}
+
+func ObtainRowFromMatrix(inputRow int, inputMatrix [][]int) []int {
+	return inputMatrix[inputRow]
+}
+
+func CalculateMatrix(matrix1, matrix2 [][]int) [][]int {
+	matrixMultiplication := [][]int{}
+
+	numberOfRows := len(matrix1)
+	numberOfColumns := len(matrix2[0])
+
+	for i := 0; i < numberOfRows; i++ {
+		row := []int{}
+		for j := 0; j < numberOfColumns; j++ {
+			row = append(row, 0)
+		}
+
+		matrixMultiplication = append(matrixMultiplication, row)
+	}
+
+	for i := 0; i < numberOfRows; i++ {
+		row := ObtainRowFromMatrix(i, matrix1)
+
+		for j := 0; j < numberOfColumns; j++ {
+			column := ObtainColumnFromMatrix(j, matrix2)
+
+			valueIJ := VectorXVector(row, column)
+			matrixMultiplication[i][j] = valueIJ
+		}
+	}
+
+	return matrixMultiplication
+}
+
+func main() {
+	matrix1 := [][]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+	}
+
+	matrix2 := [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
+		{17, 18, 19, 20},
+	}
+
+	result := CalculateMatrix(matrix1, matrix2)
+
+	fmt.Println(result)
 }
--- practice20/task2/main_test.go
+++ practice20/task2/main_test.go
@@ -1,7 +1,75 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_vectorXvector_case1(t *testing.T) {
+	vector1 := []int{1, 2, 3}
+	vector2 := []int{7, 9, 11}
+
+	expected := 58
+	actual := VectorXVector(vector1, vector2)
+
+	assert.Equal(t, expected, actual)
+}
 
 func Test_Case1(t *testing.T) {
+	input := [][]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+	}
 
+	input2 := [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
+		{17, 18, 19, 20},
+	}
+
+	expected := [][]int{
+		{175, 190, 205, 220},
+		{400, 440, 480, 520},
+		{625, 690, 755, 820},
+	}
+
+	actual := CalculateMatrix(input, input2)
+
+	assert.Equal(t, expected, actual)
+}
+
+func Test_obtainColumn_case1(t *testing.T) {
+	inputColumn := 2
+	inputMatrix := [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
+		{17, 18, 19, 20},
+	}
+
+	expected := []int{3, 7, 11, 15, 19}
+
+	actual := ObtainColumnFromMatrix(inputColumn, inputMatrix)
+	assert.Equal(t, expected, actual)
+}
+
+func Test_obtainRow_case1(t *testing.T) {
+	inputRow := 2
+	inputMatrix := [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
+		{17, 18, 19, 20},
+	}
+
+	expected := []int{9, 10, 11, 12}
+
+	actual := ObtainRowFromMatrix(inputRow, inputMatrix)
+	assert.Equal(t, expected, actual)
 }
