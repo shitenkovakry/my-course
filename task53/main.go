39871fc3e8a33d06bd50046d95ca6f3ee63ba160
kry shitenkova <shitenkovakris@mail.ru>
2023-03-23 16:50:38 +0300

    solve task53 with tests

---
+++ task53/main.go
@@ -0,0 +1,45 @@
+package main
+
+func SearchMin(array []int) int {
+	minNumber := array[0]
+
+	for indexOfArray := 0; indexOfArray < len(array); indexOfArray++ {
+		if array[indexOfArray] < minNumber {
+			minNumber = array[indexOfArray]
+		}
+	}
+
+	return minNumber
+}
+
+func SearchMax(array []int) int {
+	maxNumber := array[0]
+
+	for indexOfArray := 0; indexOfArray < len(array); indexOfArray++ {
+		if array[indexOfArray] > maxNumber {
+			maxNumber = array[indexOfArray]
+		}
+	}
+
+	return maxNumber
+}
+
+func CalculateMaxAndMin(array []int) (int, int) {
+	summaOfMax := 0
+	summaOfMin := 0
+
+	minNumber := SearchMin(array)
+	maxNumber := SearchMax(array)
+
+	for indexOfArray := 0; indexOfArray < len(array); indexOfArray++ {
+		if array[indexOfArray] != maxNumber {
+			summaOfMin += array[indexOfArray]
+		}
+
+		if array[indexOfArray] != minNumber {
+			summaOfMax += array[indexOfArray]
+		}
+	}
+
+	return summaOfMin, summaOfMax
+}
---
+++ task53/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Case1(t *testing.T) {
+	array := []int{1, 3, 5, 7, 9}
+
+	expected1 := 16
+	expected2 := 24
+	actual1, actual2 := CalculateMaxAndMin(array)
+
+	assert.Equal(t, expected1, actual1)
+	assert.Equal(t, expected2, actual2)
+}
+
+func Test_Case4(t *testing.T) {
+	array := []int{1, 3, 5, 7, 9}
+
+	expected := 1
+	actual := SearchMin(array)
+
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Case5(t *testing.T) {
+	array := []int{1, 3, 5, 7, 9}
+
+	expected := 9
+	actual := SearchMax(array)
+
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Case6(t *testing.T) {
+	array := []int{156873294, 719583602, 581240736, 605827319, 895647130}
+
+	expected1 := 2063524951
+	expected2 := 2802298787
+	actual1, actual2 := CalculateMaxAndMin(array)
+
+	assert.Equal(t, expected1, actual1)
+	assert.Equal(t, expected2, actual2)
+}
