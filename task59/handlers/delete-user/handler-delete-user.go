6e2118980a850e601d850d6704d7e1d2d2f090b2
kry shitenkova <shitenkovakris@mail.ru>
2023-04-15 17:42:24 +0300

    create interface for handlerDeleteUser

--- task59/handlers/delete-user/handler-delete-user.go
+++ task59/handlers/delete-user/handler-delete-user.go
@@ -1,8 +1,16 @@
 package deleteuser
 
+import (
+	"curse/task59/models"
+)
+
 type DeleteUser struct {
 	Email   string `json:"email"`
 	Name    string `json:"name"`
 	Age     int    `json:"age"`
 	Friends []int  `json:"friends"`
+}
+
+type UserActionsForHandlerDeleteUser interface {
+	Delete(userID int) (*models.Users, error)
 }
