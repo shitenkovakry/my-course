79275aed8e4e0f5e6b3f1a0efdef7b4781c5231a
kry shitenkova <shitenkovakris@mail.ru>
2023-04-15 19:00:35 +0300

    add inteface for handlerMakeFriend

--- task59/handlers/make-friend/handler-make-friend.go
+++ task59/handlers/make-friend/handler-make-friend.go
@@ -1,6 +1,12 @@
 package makefriend
 
+import "curse/task59/models"
+
 type MakeFriend struct {
 	SourceID int `json:"source_id"`
 	TargetID int `json:"target_id"`
+}
+
+type UserActionsForHandlerMakeFriendForUsers interface {
+	MakeFriend(sourceID, targetID int) (*models.User, *models.User, error)
 }
