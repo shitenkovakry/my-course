b3b5c39eafea3acbe7452b54250d30806ba6ed94
kry shitenkova <shitenkovakris@mail.ru>
2023-04-15 19:02:34 +0300

    add struct HandlerForMakeFriendForUsers

--- task59/handlers/make-friend/handler-make-friend.go
+++ task59/handlers/make-friend/handler-make-friend.go
@@ -1,6 +1,9 @@
 package makefriend
 
-import "curse/task59/models"
+import (
+	"curse/task59/logger"
+	"curse/task59/models"
+)
 
 type MakeFriend struct {
 	SourceID int `json:"source_id"`
@@ -9,4 +12,9 @@ type MakeFriend struct {
 
 type UserActionsForHandlerMakeFriendForUsers interface {
 	MakeFriend(sourceID, targetID int) (*models.User, *models.User, error)
+}
+
+type HandlerForMakeFriendForUsers struct {
+	log         logger.Logger
+	userActions UserActionsForHandlerMakeFriendForUsers
 }
