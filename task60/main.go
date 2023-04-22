86e53c0c7906b7442b105a2910afacca2e9f6ef8
kry shitenkova <shitenkovakris@mail.ru>
2023-04-22 15:56:49 +0300

    create functions  obtainNextID and CreateOrder

--- task60/main.go
+++ task60/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	ErrAlready = errors.New("already")
+)
 
 type Order struct {
 	ID        int
@@ -14,4 +22,41 @@ type Orders []*Order
 type Dispatcher struct {
 	orders Orders
 	log    log.Logger
+}
+
+func NewDispatcher(log log.Logger) *Dispatcher {
+	return &Dispatcher{
+		log:    log,
+		orders: make(Orders, 0),
+	}
+}
+
+func (dispatcher *Dispatcher) obtainNextID(newOrder *Order) (int, error) {
+	allOrdersInDB := dispatcher.orders
+	nextID := 0
+
+	for index := 0; index < len(allOrdersInDB); index++ {
+		order := allOrdersInDB[index]
+		if newOrder.ID == order.ID {
+			return nextID, errors.Wrapf(ErrAlready, "in db id = %s", newOrder.ID)
+		}
+
+		if nextID < order.ID {
+			nextID = order.ID
+		}
+	}
+
+	return nextID + 1, nil
+}
+
+func (dispatcher *Dispatcher) CreateOrder(newOrder *Order) (*Order, error) {
+	nextID, err := dispatcher.obtainNextID(newOrder)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot obtain next ID")
+	}
+
+	newOrder.ID = nextID
+	dispatcher.orders = append(dispatcher.orders, newOrder)
+
+	return newOrder, nil
 }
